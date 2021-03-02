8bdda193996bce02640cf2df6fe2ec46464d9abc
nokotan <kamenokonokotan@gmail.com>
2021-03-02 19:43:26 +0900

    remove auto-generated files

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WasmLib_bg_wasm_1 = require("../cpp-lib/bin/WasmLib_bg.wasm");
-console.log(WasmLib_bg_wasm_1.add(28, 29));
