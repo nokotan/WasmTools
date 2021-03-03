70b84b79afff6a92795bebde95e7a4d07df27f9f
nokotan <kamenokonokotan@gmail.com>
2021-03-03 23:13:49 +0900

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import wasmModPromise from "../cpp-lib/bin/WasmLib";
-import { WASIInstance } from "../js-bindings/WASIModule";
+import { add, sub, memory } from "../cpp-lib/bin/WasmLib.wasm";
+import { fs, wasiInstance } from "../js-bindings/WASIModule";
 
-async function main() {
-    const wasmMod = await wasmModPromise;
-    WASIInstance.instance.setMemory(wasmMod.memory);
+function preMain() {
+    // Redirect WASI's stdout to `console.log`.
+    const textDecoder = new TextDecoder();
 
-    console.log(wasmMod.add(24, 74));
-    console.log(wasmMod.sub(24, 74));
+    fs.volume.fds[1].node.write = (buf, off, len, pos) => {
+        console.log(textDecoder.decode(buf.buffer));
+        return len ?? 0;
+    };
 
-    console.log(await WASIInstance.fs.getStdOut());
+    // Pass WemAssembly.Memory instance to WASI default instance
+    // so that WASI works properly. 
+    wasiInstance.setMemory(memory);
 }
 
+function main() {
+    console.log(add(24, 74));
+    sub(24, 74);
+}
+
+preMain();
 main();
